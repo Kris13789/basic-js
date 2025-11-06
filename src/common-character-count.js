69c63af44e70889ffcd30712ccfea19e762edaf9
Kristina <kristina.podolyako.mih@gmail.com>
2025-11-06 18:17:24 +0100

    feat: solve task delete-digit

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -12,9 +12,26 @@ const { NotImplementedError } = require('../lib');
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
 
-function getCommonCharacterCount(/* s1, s2 */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function getCommonCharacterCount(s1, s2) {
+  const countChars = (str) => {
+    const count = {};
+    for (const char of str) {
+      count[char] = (count[char] || 0) + 1;
+    }
+    return count;
+  };
+
+  const count1 = countChars(s1);
+  const count2 = countChars(s2);
+
+  let commonCount = 0;
+  for (const char in count1) {
+    if (count2[char]) {
+      commonCount += Math.min(count1[char], count2[char]);
+    }
+  }
+
+  return commonCount;
 }
 
 module.exports = {
