61a6462734cdd69e54530db39b96ea6667104266
Kristina <kristina.podolyako.mih@gmail.com>
2025-11-06 21:36:54 +0100

    feat: add funciton encode-line

--- src/encode-line.js
+++ src/encode-line.js
@@ -11,9 +11,19 @@ const { NotImplementedError } = require('../lib');
  *
  */
 
-function encodeLine(/* str */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function encodeLine(str) {
+ let encodedString = '';
+ let count = 1;
+
+ for (let i = 1; i <= str.length; i++) {
+  if (str[i] === str[i - 1]) {
+    count++;
+  } else {
+    encodedString += (count > 1 ? count: '') + str[i - 1];
+    count = 1;
+  }
+ }
+return encodedString;
 }
 
 module.exports = {
