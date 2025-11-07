51f08f2a3c47e912456a3977167c488e3e8600d6
Kristina <kristina.podolyako.mih@gmail.com>
2025-11-07 14:21:05 +0100

    feat: add fucntion Vigenere Cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,17 +19,69 @@ const { NotImplementedError } = require('../lib');
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  *
  */
+
 class VigenereCipheringMachine {
-  encrypt() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  constructor(isDirect = true) {
+    this.isDirect = isDirect;
   }
 
-  decrypt() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  encrypt(message, key) {
+    if (!message || !key) throw new Error('Incorrect arguments!');
+
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+
+    let result = '';
+    let keyIndex = 0;
+
+    for (let i = 0; i < message.length; i++) {
+      const char = message[i];
+      if (char >= 'A' && char <= 'Z') {
+        const messageCode = char.charCodeAt(0) - 65;
+        const keyCode = key[keyIndex % key.length].charCodeAt(0) - 65;
+        const encryptedChar = String.fromCharCode(((messageCode + keyCode) % 26) + 65);
+        result += encryptedChar;
+        keyIndex++;
+      } else {
+        result += char;
+      }
+    }
+
+    if (!this.isDirect) result = result.split('').reverse().join('');
+    return result;
+  }
+
+  decrypt(encryptedMessage, key) {
+    if (!encryptedMessage || !key) throw new Error('Incorrect arguments!');
+
+    encryptedMessage = encryptedMessage.toUpperCase();
+    key = key.toUpperCase();
+
+    let result = '';
+    let keyIndex = 0;
+
+    for (let i = 0; i < encryptedMessage.length; i++) {
+      const char = encryptedMessage[i];
+      if (char >= 'A' && char <= 'Z') {
+        const messageCode = char.charCodeAt(0) - 65;
+        const keyCode = key[keyIndex % key.length].charCodeAt(0) - 65;
+        const decryptedChar = String.fromCharCode(((messageCode - keyCode + 26) % 26) + 65);
+        result += decryptedChar;
+        keyIndex++;
+      } else {
+        result += char;
+      }
+    }
+
+    if (!this.isDirect) result = result.split('').reverse().join('');
+    return result;
   }
 }
+
+module.exports = {
+  VigenereCipheringMachine,
+};
+
 
 module.exports = {
   directMachine: new VigenereCipheringMachine(),
